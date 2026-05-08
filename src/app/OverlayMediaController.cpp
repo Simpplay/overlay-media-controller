dc745561113d7ad92d7ff70c97dd506639aa8bce
Simpplay <Simpplay32@gmail.com>
2026-05-08 11:34:45 -0500

    feat!: remove ffmpeg dependency. Prefer to use webview to flexibility

--- src/app/OverlayMediaController.cpp
+++ src/app/OverlayMediaController.cpp
@@ -7,10 +7,6 @@
 
 #include "core/event/events/ExitApplicationRequestedEvent.hpp"
 
-#ifdef OMC_HAS_FFMPEG
-#include "infra/media/FfmpegMediaPlayer.hpp"
-#endif
-
 namespace omc::application
 {
 	void OverlayMediaController::initialize()
@@ -22,13 +18,6 @@ namespace omc::application
 		eventBus.subscribe<omc::event::ExitApplicationRequestedEvent>([this](const omc::event::ExitApplicationRequestedEvent& event) {
 			close();
 		});
-
-		#ifdef OMC_HAS_FFMPEG
-		mediaPlayer = std::make_shared<omc::infra::FfmpegMediaPlayer>(eventBus, &threadPool);
-		mediaManager = std::make_unique<omc::media::MediaManager>(eventBus, mediaPlayer);
-#else
-		std::cout << "FFmpeg not found. Media playback is disabled.\n";
-#endif
 
 		uiManager.init(&threadPool);
 
--- src/modules/ui/UiManager.hpp
+++ src/modules/ui/UiManager.hpp
@@ -5,7 +5,6 @@
 #include "core/event/EventBus.hpp"
 
 #include "core/event/events/WindowOpenRequestedEvent.hpp"
-#include "core/event/events/FrameReadyEvent.hpp"
 #include "UiWindow.hpp"
 #include "UiTypes.hpp"
 #include "infra/window/Win32Renderer.hpp"
@@ -13,7 +12,6 @@
 #include "shared/threading/ThreadPool.hpp"
 
 #include "modules/ui/windows/TestWindow.hpp"
-#include "modules/ui/windows/MediaWindow.hpp"
 #include "modules/ui/windows/WebViewWindow.hpp"
 
 namespace omc::ui
@@ -25,10 +23,6 @@ namespace omc::ui
 		{
 			eventBus.subscribe<omc::event::WindowOpenRequestedEvent>([this](const omc::event::WindowOpenRequestedEvent& event) {
 				handleWindowOpenRequestedEvent(event);
-			});
-
-			eventBus.subscribe<omc::event::FrameReadyEvent>([this](const omc::event::FrameReadyEvent& event) {
-				handleFrameReadyEvent(event);
 			});
 
 			printf("UiManager initialized..\n");
@@ -47,9 +41,6 @@ namespace omc::ui
 				eventBus.emit(omc::event::WindowOpenRequestedEvent(testWindow));
 			}
 
-			//omc::ui::window::MediaWindow mediaWindow(1, eventBus);
-			//eventBus.emit(omc::event::WindowOpenRequestedEvent(mediaWindow));
-
 			// Here create a webview window and navigate to a URL, e.g.: youtube.com, and test if it renders correctly and is interactive.
 			eventBus.emit(omc::event::WindowOpenRequestedEvent{
 				omc::ui::window::WebViewWindow("https://youtube.com", {200, 150}, {960, 640})
@@ -60,7 +51,6 @@ namespace omc::ui
 		void update();
 
 	private:
-		void handleFrameReadyEvent(const omc::event::FrameReadyEvent& event);
 		void handleWindowOpenRequestedEvent(const omc::event::WindowOpenRequestedEvent& event);
 		std::vector<std::unique_ptr<omc::ui::window::UiWindow>> windows;
 
